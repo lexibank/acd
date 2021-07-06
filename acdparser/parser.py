54132764f57005ed1e46be8e5956c952b0b39405
Robert Forkel <xrotwang@googlemail.com>
2021-07-06 20:11:41 +0200

    code cleanup

--- acdparser/parser.py
+++ acdparser/parser.py
@@ -65,8 +65,12 @@ class Parser:
                 o = self.__cls__.from_html(e)
                 if o:
                     yield o
-                else:
-                    print('skipping: {}'.format(str(e)[:50]))
+
+
+class EtymonParser(Parser):
+    __glob__ = 'acd-s_*.htm'
+    __cls__ = Etymon
+    __tag__ = ('table', ('settable', 'SettableF'))  # tables class=entrytable + p class=setnote
 
 
 class WordParser(Parser):
@@ -108,56 +112,6 @@ class SourceParser(Parser):
                 elif e['class'][0] == 'Bibline2':
                     assert author
                     yield self.__cls__(html=e, author=author, bibline2=True)
-
-
-class EtymonParser(Parser):
-    __glob__ = 'acd-s_*.htm'
-    __cls__ = Etymon
-    __tag__ = ('table', ('settable', 'SettableF'))  # tables class=entrytable + p class=setnote
-    """
-    <p class="setline">
-      <span class="key">*tektek₁</span>
-      <span class="setline">
-        <a class="setline" href="acd-ak_c.htm#chopping">chopping </a>
-        <a class="setline" href="acd-ak_t.htm#to">to </a>
-        <a class="setline" href="acd-ak_p.htm#piece">pieces, </a>
-        <a class="setline" href="acd-ak_c.htm#cutting">cutting </a>
-        <a class="setline" href="acd-ak_u.htm#up"> up, </a>
-        as
-        <a class="setline" href="acd-ak_m.htm#meat">meat </a>
-        <a class="setline" href="acd-ak_o.htm#or">or </a> <a class="setline" href="acd-ak_v.htm#vegetable">vegetables</a>
-      </span>
-    </p>
-    <a name="8913"></a>
-    <p></p><table class="entrytable"><tbody><tr><td class="entrytable">
-    <p class="pidno">8913</p>
-    <a name="tektek₁"></a><p class="pLang"><span class="pcode">PAN</span> &nbsp; &nbsp; <span class="lineform">*tektek₁ </span><span class="linegloss"><a class="setline" href="acd-ak_c.htm#chopping">chopping </a> <a class="setline" href="acd-ak_t.htm#to">to </a> <a class="setline" href="acd-ak_p.htm#piece">pieces, </a> <a class="setline" href="acd-ak_c.htm#cutting">cutting </a> <a class="setline" href="acd-ak_u.htm#up"> up, </a> as  <a class="setline" href="acd-ak_m.htm#meat">meat </a> <a class="setline" href="acd-ak_o.htm#or">or </a> <a class="setline" href="acd-ak_v.htm#vegetable">vegetables</a> </span>
-    </p>
-    <table class="forms" width="90%" align="center">
-    <tbody><tr valign="top">
-    <td class="group">Formosan</td></tr>
-    <tr valign="top"><td class="lg"><a href="acd-l_S.htm#Saisiyat"><span class="lg">Saisiyat</span></a></td>
-    <td class="formuni">təktək</td><td class="gloss">to chop wood</td></tr>
-    <tr valign="top">
-
-    <p class="setline"><span class="key">*-i₁</span> <span class="setline">imperative suffix</span></p>
-    <a name="3033"></a>
-    <p><table class="entrytable"><tbody><tr><td class="entrytable">
-    <p class="pidno">3033</p>
-    <a name="-i₁"></a><p class="pLang"><span class="pcode">PAN</span> &nbsp; &nbsp; <span class="lineform">*-i₁ </span><span class="linegloss">imperative suffix </span>
-    <table class="forms" width="90%" align="center">
-    <tbody><tr valign="top">
-    <td class="group">Formosan</td></tr>
-    <tr valign="top"><td class="lg">Atayal</td>
-    <td class="formuni">-i</td><td class="gloss">suffix forming first passive negatable indicative and imperative from the reduced stem</td></tr>
-    ...
-    <p class="setnote"><span class="note">Note: &nbsp; </span><span class="bib"><a class="bib" href="acd-bib.htm#Egerod">Egerod (1965)</a></span> describes three "passives'' for <span class="lg">Atayal</span>: (1) a "relational passive'', (2) an "indefinite passive'', and (3) a "definite passive''.  The definite passive can occur in any of five aspects, of which the "neutral definite passive'' is marked by <span class="wd">-an</span>, while the "negatable neutral definite passive'' and the "imperative definite passive'' are marked by <span class="wd">-i</span>.  The paradigmatic alternation of <span class="wd">-an</span> and <span class="wd">-i</span> thus appears to be common to at least <span class="lg">Atayal</span> and <span class="lg">Bikol</span>.  The present affix evidently is identical to what <span class="bib"><a class="bib" href="acd-bib.htm#Wolff">Wolff (1973:73)</a></span> called the "local passive dependent'' construction. It seems to have marked imperatives of a certain type, but may have been used with a wider range of construction types than I have indicated here.
-</p>
-    """
-    def __iter__(self):
-        for html in self.iter_html():
-            for e in html.find_all(self.__tag__[0], class_=self.__tag__[1]):
-                yield self.__cls__.from_html(e)
 
 
 class LanguageParser(Parser):
