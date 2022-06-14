9ced571258ba77ad56b85ffc3c4fbf0e62275e2e
Robert Forkel <xrotwang@googlemail.com>
2022-06-14 14:21:53 +0200

    added merap data

--- acdcommands/addlanguage.py
+++ acdcommands/addlanguage.py
@@ -46,6 +46,18 @@ def run(args):
         p = args.datadir.joinpath('{}.tsv'.format(name))
         assert p.exists()
         data[name] = list(reader(p, delimiter='\t', dicts=True))
+    #
+    # get all non-inferred protoforms, keyed by associated form['Value']
+    #
+    cldf = ds.cldf_reader()
+    forms_by_id = {f['ID']: f for f in cldf['FormTable']}
+    protoforms = {}
+    for pf in cldf['protoforms.csv']:
+        if not pf['Inferred']:
+            # FIXME: add Proto_Language to the key!?
+            protoforms[forms_by_id[pf['Form_ID']]['Value']] = pf
+            protoforms[pf['ID']] = pf
+
     with UpdateWriter(ds.cldf_specs(), args, ds) as writer:
         assert len(data['metadata']) == 1
         for lang in data['metadata']:
@@ -57,25 +69,41 @@ def run(args):
             writer.add_language(
                 ID=lid,
                 Name=lang['name'],
+                Group=lang['group'].upper(),
+                ISO639P3code=lang['ISO639P3code'],
+                Location=lang['location'],
+                Alias=lang['Alias'],
+                Source=[lang['source']],
+                Longitude=float(lang['lon']),
+                Latitude=float(lang['lat']),
             )
             break
         for form in data['forms']:
-            cid = hash(form['merap gloss'])
+            cid = hash(form['merap gloss'] or form['gloss'])
             if cid not in writer._obj_index['ParameterTable']:
                 writer.add_concept(
                     ID=cid,
                     Name=form['merap gloss'],
                 )
 
+            pf = protoforms[form['pfid']] if form['pfid'] else protoforms[form['reconstruction']]
+            #
+            # FIXME: add disambiguation markers!
+            #
+            if pf['Proto_Language'] != form['proto level'].upper():
+                print('{}: {} vs. {}'.format(form['reconstruction'], pf['Proto_Language'], form['proto level'].upper()))
+                #
+                # FIXME: We might have to deepen the reconstruction level!?
+                #
+                pass
             for lexeme in writer.add_forms_from_value(
                 Language_ID=lid,
                 Parameter_ID=cid,
                 Value=form['Merap'],
             ):
                 writer.add_cognate(
-                    Form_ID=lexeme['ID'],
-                    Form=lexeme,
-                    Reconstruction_ID=str(sid),
-                    Cognateset_ID=str(etymon['id']),
-                    Proto_Language=pl,
+                    lexeme=lexeme,
+                    Reconstruction_ID=pf['ID'],
+                    Cognateset_ID=pf['Cognateset_ID'],
+                    Proto_Language=form['proto level'].upper(),
                 )
